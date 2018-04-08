3f06b96195e94eeff59618a6fa81a3bbe7ddc621
aaarel <aaarel@gmail.com>
2018-04-09 01:52:30 +0300

    added back /input dir to VCS to backup files needed for the system

--- src/main/java/Model/Constants.java
+++ src/main/java/Model/Constants.java
@@ -5,38 +5,36 @@ package Model;
  */
 public class Constants {
 
-	public static String SHEET_NAME = "Sheet1";
+	public static final String SHEET_NAME = "Sheet1";
 
 	//regular expressions
-	public static String REGEX_ONLY_NUMBERS = "[0-9]\\+"; //
-	public static String REGEX_ONLY_NUMBERS_A_TO_Z_LETTERS = "\\^[a-z0-9]\\+$/i";
-	public static String REGEX_ILLEGAL_CHARS = "[/.,!@\\\\#$>:;|<%^&?*()-]";
-	//public static String REGEX_TMP = "[\\/:*?.,!@#$%^&-()\"<>|]";
+	public static final String REGEX_ONLY_NUMBERS = "[0-9]\\+";
+	public static final String REGEX_FILTER_UNWANTED_CHARS = "[\\-\\+\\.\\^:,/]";
+	public static final String REGEX_ONLY_NUMBERS_A_TO_Z_LETTERS = "\\^[a-z0-9]\\+$/i";
+	public static final String REGEX_ILLEGAL_CHARS = "[/.,!@\\\\#$>:;|<%^&?*()-]";
+	//public static final String REGEX_TMP = "[\\/:*?.,!@#$%^&-()\"<>|]";
 
 	//DHL invoice file
 	public static int FIRST_ROW_NUM = 0;
-	public static String CUSTOMER_COLUMN_NAME = "Shipper_nm";
-	public static String REF_NUM_COLUMN_NAME = "Reference_no";
-	public static String BLANK = "";
-	public static String FREIGHT = "Frieght";
-	public static String COUNTRY_COL = "countryname";
-	public static String ZONE_NUM_COL = "ISRAEL ZONE";
-	public static String WEIGHT_COL = "Wght";
-	public static String DES_COL = "Destination";
+	public static int FIRST_SHHET_NUM = 0;
+	public static final String CUSTOMER_COLUMN_NAME = "Shipper_nm";
+	public static final String REF_NUM_COLUMN_NAME = "Reference_no";
+	public static final String BLANK = "";
+	public static final String FREIGHT = "Frieght";
+	public static final String COUNTRY_COL = "countryname";
+	public static final String ZONE_NUM_COL = "ISRAEL ZONE";
+	public static final String WEIGHT_COL = "Wght";
+	public static final String DES_COL = "Destination";
 
 	//customer price list files
-	public static String PL_FILE_ENDING = "price list.xlsx";
+	public static final String PL_FILE_ENDING = "price list.xlsx";
 	public static int ZONE_OFFSET = 3;
 	public static double WEIGHT_MULTIPLIER = 0.5;
 
-	//customersNamesToIdsMapping.xls file
-	public static int CUST_NAME_COL = 0;
-	public static int CUST_ID_COL = 1;
-	public static int CUST_TYPE_COL = 2;
 
 	//Errors
-	public static String COLUMN_NOT_FOUND_ERROR = "Couldn't find column name";
-	public static String FUEL_SURCHARGE_NOT_IN_RANGE = "FuelSurchargePercent not in the range of 0-1";
-	public static String COUNTRY_CODE_ERROR = "country code does not exist in table";
+	public static final String COLUMN_NOT_FOUND_ERROR = "Couldn't find column name";
+	public static final String FUEL_SURCHARGE_NOT_IN_RANGE = "FuelSurchargePercent not in the range of 0-1";
+	public static final String COUNTRY_CODE_ERROR = "country code does not exist in table";
 
 }
